aa2aded24982de5ed6031f75def714f59cc23e51
Abakar Ramazanov <ramazanov.null@gmail.com>
2022-05-03 16:39:38 +0300

    fix test

--- hw01_hello_otus/main_test.go
+++ hw01_hello_otus/main_test.go
@@ -1,11 +1,21 @@
 package main
 
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
 func TestReverse(t *testing.T) {
 	tests := []struct {
 		input    string
 		expected string
 	}{
 		{"Hello", "olleH"},
+		{"OTUS", "SUTO"},
+		{"Hello, OTUS!", "!SUTO ,olleH"},
+		{"abcd", "dcba"},
+		{" 123 =", "= 321 "},
+		{"+-*/", "/*-+"},
 	}
 
 	for _, tc := range tests {
