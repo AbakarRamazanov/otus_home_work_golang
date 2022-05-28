39124588772515768a1000998cdfa24e992f1042
Abakar Ramazanov <ramazanov.null@gmail.com>
2022-05-28 10:18:43 +0300

    hw03 completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,62 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"math"
+	"sort"
+	"strings"
+)
+
+type wordCount struct {
+	word  string
+	count int
+}
+
+type contWC struct {
+	wordCounts []wordCount
+}
+
+func (c contWC) Len() int {
+	return len(c.wordCounts)
+}
+
+func (c contWC) Less(i, j int) bool {
+	if c.wordCounts[i].count == c.wordCounts[j].count {
+		if strings.Compare(c.wordCounts[i].word, c.wordCounts[j].word) > 0 {
+			return false
+		} else {
+			return true
+		}
+	}
+	return c.wordCounts[i].count > c.wordCounts[j].count
+}
+
+func (c contWC) Swap(i, j int) {
+	b := c.wordCounts[j]
+	c.wordCounts[j] = c.wordCounts[i]
+	c.wordCounts[i] = b
+}
+
+func (c contWC) getTopWords() []string {
+	min := int(math.Min(float64(len(c.wordCounts)), 10))
+	words := make([]string, min)
+	for i := 0; i < min; i++ {
+		words[i] = c.wordCounts[i].word
+	}
+	return words
+}
+
+func Top10(input string) []string {
+	words := strings.Fields(input)
+	m := make(map[string]int, 0)
+	for _, word := range words {
+		m[word]++
+	}
+	wordCounts := make([]wordCount, 0, len(m))
+	for key, value := range m {
+		wordCounts = append(wordCounts, wordCount{word: key, count: value})
+	}
+	container := contWC{wordCounts: wordCounts}
+	sort.Sort(container)
 	// Place your code here.
-	return nil
+	return container.getTopWords()
 }
