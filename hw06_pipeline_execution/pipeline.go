4fd35c7d24186e4837477ee138f2978348ec92fe
Abakar Ramazanov <ramazanov.null@gmail.com>
2022-06-23 13:55:18 +0300

    send

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,57 +9,9 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// out := make(chan interface{})
-	// go func() {
-	// 	defer close(out)
-	// 	for {
-	// 		select {
-	// 		default:
-	// 			out <- runStages(done, in, stages...)
-	// 			// fmt.Println(i)
-	// 		case <-done:
-	// 			return
-	// 		}
-	// 	}
-	// }()
-	// Place your code here.
-	// return runStages2(done, in, stages...)
-	var out In = in
-	for _, stage := range stages {
-		out = proccessStage(done, stage(out))
-	}
-	return out
-}
-
-func runStages(done In, in In, stages ...Stage) Out {
-	out := make(chan interface{})
-	s := stages[0]
-	go func() {
-		defer close(out)
-		ino := proccessStage(done, s(in))
-		for {
-			select {
-			case v, ok := <-ino:
-				if ok {
-					out <- v
-				} else {
-					return
-				}
-			case <-done:
-				return
-			}
-		}
-	}()
-	if len(stages) > 1 {
-		return runStages(done, out, stages[1:]...)
-	}
-	return out
-}
-
-func runStages2(done In, in In, stages ...Stage) Out {
-	var out In = in
-	for _, stage := range stages {
-		out = proccessStage(done, stage(out))
+	out := in
+	for i := 0; i < len(stages); i++ {
+		out = proccessStage(done, stages[i](out))
 	}
 	return out
 }
