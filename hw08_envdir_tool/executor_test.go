35cd0fcabb62b8d3544221f1ea544b475ce15606
Abakar Ramazanov <abakar.r.ramazanov@yandex.ru>
2022-07-15 22:51:22 +0300

    add tests

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,5 +3,13 @@ package main
 import "testing"
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	var envTest = Environment{
+		"BAR":   EnvValue{"bar", false},
+		"EMPTY": EnvValue{"", false},
+		"FOO":   EnvValue{"   foo\nwith new line", false},
+		"HELLO": EnvValue{`"hello"`, false},
+		"UNSET": EnvValue{"", true},
+	}
+	var cmd = []string{"echo", "-n", `$(BAR)`, "$(HELLO)"}
+	RunCmd(cmd, envTest)
 }
