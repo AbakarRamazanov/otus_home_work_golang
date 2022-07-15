cda91cc33cfb9f1a62633d5f37691c7581c0c0fc
Abakar Ramazanov <abakar.r.ramazanov@yandex.ru>
2022-07-15 22:01:26 +0300

    check resolve

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
 	// Place your code here.
-	return
+	for variable, value := range env {
+		if value.NeedRemove {
+			os.Unsetenv(variable)
+		} else {
+			os.Setenv(variable, value.Value)
+		}
+	}
+	var out bytes.Buffer
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Stdout = &out
+	err := command.Run()
+	if err != nil {
+		return -1
+	}
+	fmt.Println(out.String())
+	return command.ProcessState.ExitCode()
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	RunCmd(os.Args[2:], env)
+	// fmt.Println()
 	// Place your code here.
 }
